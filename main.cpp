4d67701692ff85cbcf9a1f540835eaa184208d3c
dumbyAlek <shams.aalok@northsouth.edu>
2025-07-14 02:13:40 +0600

    .txt --> .edges

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 int main() { 
 	vector<vector<int>> *adjacency = new vector<vector<int>>(); 
-	string filename = "/home/sujat/projects/cse491/graphs/graph1.txt";
+	string filename = "/home/sujat/projects/cse491/graphs/graph1.edges";
 	build_adjacency_list(filename, adjacency);
 }
